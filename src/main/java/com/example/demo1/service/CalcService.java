90a9f5411f68da524aa619ee2f30a33f9286c758
denis-dorofeev61224 <denis_dorofeev_1977@mail.ru>
2024-10-15 12:46:45 +0300

    Modification done

--- src/main/java/com/example/demo1/service/CalcService.java
+++ src/main/java/com/example/demo1/service/CalcService.java
@@ -1,7 +1,14 @@
 package com.example.demo1.service;
 public interface CalcService {
-    int add(int a, int b);
+   /*int add(int a, int b);
     int subtract(int a, int b);
     int multiply(int a, int b);
-    int divide(int a, int b);
+    int divide(int a, int b);*/
+    //закомитил чтобы были видны изменения
+   Integer add(Integer a, Integer b);
+    Integer subtract(Integer a, Integer b);
+    Integer multiply(Integer a, Integer b);
+    Integer divide(Integer a, Integer b);
+
+
 }
--- src/main/java/com/example/demo1/service/CalculatorServiceImpl.java
+++ src/main/java/com/example/demo1/service/CalculatorServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo1.service;
 import org.springframework.stereotype.Service;
 
+/*
 @Service  // Аннотация @Service делает этот класс бином в Spring-контейнере
 public class CalculatorServiceImpl implements CalcService {
     @Override
@@ -20,6 +21,34 @@ public class CalculatorServiceImpl implements CalcService {
 
     @Override
     public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+}
+*/
+
+@Service
+public class CalculatorServiceImpl implements CalcService {
+
+    @Override
+    public Integer add(Integer a, Integer b) {
+        return a + b;
+    }
+
+    @Override
+    public Integer subtract(Integer a, Integer b) {
+        return a - b;
+    }
+
+    @Override
+    public Integer multiply(Integer a, Integer b) {
+        return a * b;
+    }
+
+    @Override
+    public Integer divide(Integer a, Integer b) {
         if (b == 0) {
             throw new IllegalArgumentException("Division by zero is not allowed.");
         }
